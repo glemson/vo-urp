210fbb7a1a6308fd07136fa55ef1d703ec39b4bd
bourges.laurent@gmail.com <bourges.laurent@gmail.com@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-04-28 05:54:20 +0000

    EMPTY implementation to allow build process to finish properly

---
+++ trunk/src/org/ivoa/dm/model/RootEntityObject.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ivoa.dm.model;
+
+import java.util.Map;
+
+/**
+ *
+ * @author laurent
+ */
+public class RootEntityObject extends MetadataObject {
+
+    public void setOwner(String name) {
+
+    }
+
+        public void setUpdateUser(String name) {
+
+    }
+
+          protected StringBuilder deepToString(final StringBuilder sb, final boolean isDeep,
+                                                final Map<MetadataElement, Object> ids) {
+                                                return null;
+          }
+}
