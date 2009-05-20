7eba7e6f2a32e50c789a89d19568da13da2863f7
bourges.laurent@gmail.com <bourges.laurent@gmail.com@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-05-20 06:08:39 +0000

    unused old code moved

---
+++ deprecated-src/org/ivoa/dm/semantics/Concept.java
@@ -0,0 +1,38 @@
+/*
+ * Concept.java
+ *
+ * Author Gerard Lemson
+ * Created on 11 May 2008
+ */
+package org.ivoa.dm.semantics;
+
+
+/**
+ * TODO : Class Description
+ *
+ * @author laurent bourges (voparis) / Gerard Lemson (mpe)
+ */
+public class Concept {
+  //~ Members ----------------------------------------------------------------------------------------------------------
+
+  /** The ontology this concept belongs to */
+  private Ontology ontology;
+  /** The name by which this concept is known in its ontology. Name is unique in its ontology. */
+  private String name;
+  /** The globally unique URI by which this concept is known in it's ontology */
+  private String URI;
+  /** An English language description of this concept */
+  private String description_en;
+
+  //~ Constructors -----------------------------------------------------------------------------------------------------
+
+  /** Protected constructor, concepts cna only be created by the OntologyFactory. */
+  protected Concept(final Ontology _ontology) {
+    if (_ontology == null) {
+      throw new IllegalArgumentException("A concept must be created with its ontology.");
+    }
+
+    this.ontology = _ontology;
+  }
+}
+//~ End of file --------------------------------------------------------------------------------------------------------
---
+++ deprecated-src/org/ivoa/dm/semantics/Thesaurus.java
@@ -0,0 +1,17 @@
+/*
+ * Thesaurus.java
+ *
+ * Author Gerard Lemson
+ * Created on 11 May 2008
+ */
+package org.ivoa.dm.semantics;
+
+
+/**
+ * TODO : Class Description
+ *
+ * @author laurent bourges (voparis) / Gerard Lemson (mpe)
+ */
+public class Thesaurus {
+}
+//~ End of file --------------------------------------------------------------------------------------------------------
