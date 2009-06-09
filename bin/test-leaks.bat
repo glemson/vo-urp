8a13b045f35d3048be33600a70d45c7d69acf653
bourges.laurent@gmail.com <bourges.laurent@gmail.com@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-06-09 06:18:29 +0000

    tomcat memory leak test

---
+++ bin/test-leaks.bat
@@ -0,0 +1,7 @@
+REM wget --help > wget.txt
+
+wget -v -S --no-cache --no-http-keep-alive --user=admin --password=llooo --input-file=inputURL-deploy.txt  -O tmp\output-deploy.html
+
+REM wget -v -S --no-cache --no-http-keep-alive --user=admin --password=llooo --input-file=inputURL-use.txt -O tmp\output-use.html
+
+pause
