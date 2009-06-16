14609f6630055ce98be2e3646d2ff62692437f42
bourges.laurent <bourges.laurent@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-06-16 13:18:26 +0000

    new Configurable interface

---
+++ src/org/ivoa/conf/Configurable.java
@@ -0,0 +1,16 @@
+package org.ivoa.conf;
+
+/**
+ * This interface defines the Configuration class to use in this project and its file name
+ * 
+ * @author laurent bourges (voparis)
+ */
+public interface Configurable {
+
+  /** Configuration class name */
+  public static final String CONFIGURATION_CLASS = "org.ivoa.conf.RuntimeConfiguration";
+
+  /** file name for property file */
+  public static final String CONFIGURATION_FILE = "runtime.properties";
+
+}
