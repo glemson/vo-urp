aef59d7fea9933d8d2b3a14c569a2189bde6f8d2
bourges.laurent@gmail.com <bourges.laurent@gmail.com@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-07-30 06:28:44 +0000

    updated XML catalog resolver

--- src/org/ivoa/conf/Configuration.java
+++ src/org/ivoa/conf/Configuration.java
@@ -3,6 +3,7 @@ package org.ivoa.conf;
 import org.ivoa.util.CollectionUtils;
 import org.ivoa.util.ReflectionUtils;
 
+
 /**
  * This class acts as a singleton to store the global Configuration for the application :
  * <ul>
@@ -14,200 +15,204 @@ import org.ivoa.util.ReflectionUtils;
  * @author Laurent Bourges (voparis) / Gerard Lemson (mpe)
  */
 public class Configuration extends PropertyHolder implements Configurable {
-    //~ Constants --------------------------------------------------------------------------------------------------------
+  //~ Constants --------------------------------------------------------------------------------------------------------
 
-    /** serial UID for Serializable interface */
-    private static final long serialVersionUID = 1L;
-    /** singleton instance (java 5 memory model) */
-    private static volatile Configuration instance = null;
+  /** serial UID for Serializable interface */
+  private static final long serialVersionUID = 1L;
+  /** singleton instance (java 5 memory model) */
+  private static volatile Configuration instance = null;
 
-    /* property keys */
-    /** keyword for application title */
-    public static final String APP_TITLE = "project.title";
-    /** keyword for application vendor */
-    public static final String APP_VENDOR = "project.vendor";
-    /** keyword for application version */
-    public static final String APP_VERSION = "project.version";
-    /**
-     * keyword for service ivo id. The IVO Identifier by which the data access service is registered. Used as
-     * prefix for ivo-id-s of all objects.
-     */
-    public static final String SERVICE_IVOID = "service.ivoid";
-    /** keyword for java Main Class */
-    public static final String MAIN_CLASS = "main.class";
-    /** keyword for test mode */
-    public static final String MODE_TEST = "mode.test";
+  /* property keys */
+  /** keyword for application title */
+  public static final String APP_TITLE = "project.title";
+  /** keyword for application vendor */
+  public static final String APP_VENDOR = "project.vendor";
+  /** keyword for application version */
+  public static final String APP_VERSION = "project.version";
+  /**
+   * keyword for service ivo id. The IVO Identifier by which the data access service is registered. Used as
+   * prefix for ivo-id-s of all objects.
+   */
+  public static final String SERVICE_IVOID = "service.ivoid";
+  /** keyword for java Main Class */
+  public static final String MAIN_CLASS = "main.class";
+  /** keyword for test mode */
+  public static final String MODE_TEST = "mode.test";
+  //~ Members ----------------------------------------------------------------------------------------------------------
+  /** As the prefix is needed often, store it explicitly. */
+  private String ivoIdPrefix = "";
+  /** os type */
+  private final OSEnum os;
 
-    //~ Members ----------------------------------------------------------------------------------------------------------
-    /** As the prefix is needed often, store it explicitly. */
-    private String ivoIdPrefix = "";
-    /** os type */
-    private final OSEnum os;
+  //~ Constructors -----------------------------------------------------------------------------------------------------
+  /**
+   * public Constructor used for Introspection. Checks the OS
+   */
+  public Configuration() {
+    super();
+    this.os = checkOsName();
+  }
 
-    //~ Constructors -----------------------------------------------------------------------------------------------------
-    /**
-     * public Constructor used for Introspection. Checks the OS
-     */
-    public Configuration() {
-        super();
-        this.os = checkOsName();
+  //~ Methods ----------------------------------------------------------------------------------------------------------
+  /**
+   * Return the Configuration singleton instance
+   *
+   * @return Configuration singleton instance
+   *
+   * @throws IllegalStateException if a problem occured
+   */
+  public static final Configuration getInstance() {
+    if (instance == null) {
+      final Class<? extends Configuration> confClass = ReflectionUtils.findClass(
+          CONFIGURATION_CLASS, Configuration.class);
+
+      final Configuration conf = ReflectionUtils.newInstance(confClass);
+      conf.setPropertyFile(CONFIGURATION_FILE);
+
+      instance = prepareInstance(conf);
     }
+    return instance;
+  }
 
-    //~ Methods ----------------------------------------------------------------------------------------------------------
-    /**
-     * Return the Configuration singleton instance
-     *
-     * @return Configuration singleton instance
-     *
-     * @throws IllegalStateException if a problem occured
-     */
-    public static final Configuration getInstance() {
-        if (instance == null) {
-            final Class<? extends Configuration> confClass = ReflectionUtils.findClass(
-                    CONFIGURATION_CLASS, Configuration.class);
+  /**
+   * Pre Init event (can be overridden in child classes)
+   */
+  @Override
+  protected void preInit() {
+    // first dump properties :
+    dumpSystemProps();
+  }
 
-            final Configuration conf = ReflectionUtils.newInstance(confClass);
-            conf.setPropertyFile(CONFIGURATION_FILE);
+  /**
+   * Post Init event (can be overridden in child classes)
+   *
+   * @return true if well done
+   */
+  @Override
+  protected boolean postInit() {
+    this.ivoIdPrefix = getProperty(SERVICE_IVOID, "").trim() + "#";
 
-            instance = prepareInstance(conf);
-        }
-        return instance;
+    return true;
+  }
+
+  /**
+   * Dumps System.properties in logs (info) ...
+   */
+  private final void dumpSystemProps() {
+    if (logB.isWarnEnabled()) {
+      logB.warn("System.properties : " + CollectionUtils.toString(System.getProperties()));
     }
+  }
 
-    /**
-     * Pre Init event (can be overridden in child classes)
-     */
-    @Override
-    protected void preInit() {
-        // first dump properties :
-        dumpSystemProps();
-    }
-
-    /**
-     * Post Init event (can be overridden in child classes)
-     *
-     * @return true if well done
-     */
-    @Override
-    protected boolean postInit() {
-        this.ivoIdPrefix = getProperty(SERVICE_IVOID, "").trim() + "#";
-
-        return true;
-    }
-
-    /**
-     * Dumps System.properties in logs (info) ...
-     */
-    private final void dumpSystemProps() {
-    if (logB.isInfoEnabled()) {
-      logB.info("System.properties : " + CollectionUtils.toString(System.getProperties()));
-        }
-    }
-
-    /**
-     * Check os type
-     *
-     * @return OSEnum value
-     */
-    private final static OSEnum checkOsName() {
-        final String os = System.getProperty("os.name").toLowerCase();
+  /**
+   * Check os type
+   *
+   * @return OSEnum value
+   */
+  private final static OSEnum checkOsName() {
+    final String os = System.getProperty("os.name").toLowerCase();
 
     if (logB.isInfoEnabled()) {
       logB.info("OS : " + os);
+    }
+
+    OSEnum res = OSEnum.OS_OTHER;
+
+    if (os.contains("linux")) {
+      res = OSEnum.OS_LINUX;
+    } else {
+      if (os.contains("mac os x")) {
+        res = OSEnum.OS_MAC_OS_X;
+      } else {
+        if (os.contains("windows")) {
+          res = OSEnum.OS_MSWINDOWS;
         }
-
-        OSEnum res = OSEnum.OS_OTHER;
-
-        if (os.contains("linux")) {
-            res = OSEnum.OS_LINUX;
-        } else if (os.contains("mac os x")) {
-            res = OSEnum.OS_MAC_OS_X;
-        } else if (os.contains("windows")) {
-            res = OSEnum.OS_MSWINDOWS;
-        }
-
-        return res;
+      }
     }
 
-    /**
-     * Gives the OSEnum value
-     *
-     * @return OSEnum value
-     */
-    public final OSEnum getOs() {
-        return this.os;
-    }
+    return res;
+  }
 
-    /**
-     * Is a Mac OS X ?
-     *
-     * @return true if mac os x
-     */
-    public final boolean isMacOsX() {
-        return this.os == OSEnum.OS_MAC_OS_X;
-    }
+  /**
+   * Gives the OSEnum value
+   *
+   * @return OSEnum value
+   */
+  public final OSEnum getOs() {
+    return this.os;
+  }
 
-    /**
-     * Is linux OS ?
-     *
-     * @return true if linux os
-     */
-    public final boolean isLinux() {
-        return this.os == OSEnum.OS_LINUX;
-    }
+  /**
+   * Is a Mac OS X ?
+   *
+   * @return true if mac os x
+   */
+  public final boolean isMacOsX() {
+    return this.os == OSEnum.OS_MAC_OS_X;
+  }
 
-    /**
-     * Is Microsoft Windows OS ?
-     *
-     * @return true if MS Windows os
-     */
-    public final boolean isMSWindows() {
-        return this.os == OSEnum.OS_MSWINDOWS;
-    }
+  /**
+   * Is linux OS ?
+   *
+   * @return true if linux os
+   */
+  public final boolean isLinux() {
+    return this.os == OSEnum.OS_LINUX;
+  }
 
-    /**
-     * Is an other OS ?
-     *
-     * @return true if mac os x
-     */
-    public final boolean isOther() {
-        return this.os == OSEnum.OS_OTHER;
-    }
+  /**
+   * Is Microsoft Windows OS ?
+   *
+   * @return true if MS Windows os
+   */
+  public final boolean isMSWindows() {
+    return this.os == OSEnum.OS_MSWINDOWS;
+  }
 
-    /**
-     * Returns test mode
-     *
-     * @return test mode
-     */
-    public final boolean isTest() {
-        return this.getBoolean(MODE_TEST);
-    }
+  /**
+   * Is an other OS ?
+   *
+   * @return true if mac os x
+   */
+  public final boolean isOther() {
+    return this.os == OSEnum.OS_OTHER;
+  }
 
-    /**
-     * Gives the application title : title + ' - ' + version
-     *
-     * @return title + ' - ' + version
-     */
-    public final String getTitle() {
-        return this.getProperty(Configuration.APP_TITLE) + " - " + this.getProperty(Configuration.APP_VERSION);
-    }
+  /**
+   * Returns test mode
+   *
+   * @return test mode
+   */
+  public final boolean isTest() {
+    return this.getBoolean(MODE_TEST);
+  }
 
-    /**
-     * Gives the service IVO Id + "#" as prefix for all
-     *
-     * @return service IVO Id + "#"
-     */
-    public final String getIVOIdPrefix() {
-        return this.ivoIdPrefix;
-    }
+  /**
+   * Gives the application title : title + ' - ' + version
+   *
+   * @return title + ' - ' + version
+   */
+  public final String getTitle() {
+    return this.getProperty(Configuration.APP_TITLE) + " - " + this.getProperty(Configuration.APP_VERSION);
+  }
 
-    /**
-     * Gives the service IVO Id + "#" as prefix for all
-     *
-     * @return service IVO Id + "#"
-     */
-    protected final String getIvoIdPrefix() {
-        return this.ivoIdPrefix;
-    }
+  /**
+   * Gives the service IVO Id + "#" as prefix for all
+   *
+   * @return service IVO Id + "#"
+   */
+  public final String getIVOIdPrefix() {
+    return this.ivoIdPrefix;
+  }
+
+  /**
+   * Gives the service IVO Id + "#" as prefix for all
+   *
+   * @return service IVO Id + "#"
+   */
+  protected final String getIvoIdPrefix() {
+    return this.ivoIdPrefix;
+  }
 }
 //~ End of file --------------------------------------------------------------------------------------------------------
+
