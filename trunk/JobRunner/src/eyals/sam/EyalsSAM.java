adfed0a77972a7b3998dfeeb808973ceabca645d
bourges.laurent@gmail.com <bourges.laurent@gmail.com@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-08-26 18:51:12 +0000

    format + status OK fixed

--- trunk/JobRunner/src/eyals/sam/EyalsSAM.java
+++ trunk/JobRunner/src/eyals/sam/EyalsSAM.java
@@ -5,174 +5,173 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.ivoa.runner.FileManager;
+import org.ivoa.util.JavaUtils;
 import org.ivoa.util.runner.LocalLauncher;
 import org.ivoa.util.runner.RootContext;
 import org.ivoa.util.runner.RunContext;
 import org.ivoa.util.runner.RunState;
-import org.ivoa.util.runner.process.ProcessContext;
 import org.ivoa.web.servlet.JobServlet;
 import org.ivoa.web.servlet.JobStateException;
 
-public class EyalsSAM extends JobServlet{
 
-	// ~ the tasks
-	public static final String MAIN_TASK = "main";
-	public static final String PLOT_TASK = "plot";
-	
-	
-	public static final String FREE_PARAM_DELIMITER = "___";
-	public static final String FIXED_PARAM_DELIMITER = "%%%";
-	private String paramtemplate;
-	private List<String> freeParameters;
-	private String treesDir;
-	private String bc03Dir;
-	private String executable;
-	private String name;
-	private String[] plotCommand;
-	
-	@Override
-	protected void initialiseMainJob(RootContext rootCtx, HttpServletRequest request) throws JobStateException
-	{
-		try
-		{
-		String[] command = prepareMainTask(rootCtx.getWorkingDir(), request);
-        LocalLauncher.prepareChildJob(rootCtx, MAIN_TASK, command);
-		} catch(Exception e)
-		{
-			throw new JobStateException(e);
-		}
-	}
-	
-	
-    private String[] prepareMainTask(final String workDir, HttpServletRequest req) throws IOException {
-		// TODO Auto-generated method stub
-		String parameters = paramtemplate;
-		for(String param : freeParameters)
-		{
-			String value = req.getParameter(param);
-			// TODO validate
-			parameters = parameters.replaceAll(FREE_PARAM_DELIMITER+param+FREE_PARAM_DELIMITER, value);
-		}
-		log.debug("workDir :"+workDir);
-		
-		final String wd = workDir.replace('\\', '/');
-		log.debug("workDir repl :"+workDir);
-		
-		parameters = parameters.replaceAll(FIXED_PARAM_DELIMITER+"workingDir"+FIXED_PARAM_DELIMITER, wd);
+public class EyalsSAM extends JobServlet {
 
-		File f = new File(workDir+"/esam_params");
-  	    FileWriter w = new FileWriter(f);
-		w.write(parameters);
-		w.flush();
-		w.close();
-		
-		return new String[]{executable};
-	}
+  // ~ the tasks
+  public static final String MAIN_TASK = "main";
+  public static final String PLOT_TASK = "plot";
+  public static final String FREE_PARAM_DELIMITER = "___";
+  public static final String FIXED_PARAM_DELIMITER = "%%%";
+  private String paramtemplate;
+  private List<String> freeParameters;
+  private String treesDir;
+  private String bc03Dir;
+  private String executable;
+  private String name;
+  private String[] plotCommand;
 
-    /**
-     * Return the job default folder default/
-     * @return
-     */
-    public String getName() {
-      return name;
+  @Override
+  protected void initialiseMainJob(RootContext rootCtx, HttpServletRequest request) throws JobStateException {
+    try {
+      String[] command = prepareMainTask(rootCtx.getWorkingDir(), request);
+      LocalLauncher.prepareChildJob(rootCtx, MAIN_TASK, command);
+    } catch (Exception e) {
+      throw new JobStateException(e);
     }
+  }
 
-    protected String showJob(final HttpServletRequest request, final Integer id) {
-        String page = super.showJob(request, id);
-        
-        final RootContext ctx = (RootContext)request.getAttribute("runContext");
-        if(ctx != null) {
-	        File path; 
-	        switch(ctx.getState())
-	        {
-	        	case STATE_RUNNING:
-	        		path = new File(ctx.getWorkingDir());
-	        		break;
-	        	case STATE_FINISHED_ERROR :
-	        	case STATE_FINISHED_OK :
-	        		path = new File(FileManager.ARCHIVE, ctx.getRelativePath());
-	        		break;
-	       		default:
-	       			path = null;
-	        }
-	        request.setAttribute("resultDir", path);
+  private String[] prepareMainTask(final String workDir, HttpServletRequest req) throws IOException {
+    // TODO Auto-generated method stub
+    String parameters = paramtemplate;
+    String value;
+    for (String param : freeParameters) {
+      value = req.getParameter(param);
+
+      if (JavaUtils.isEmpty(value)) {
+        if (log.isWarnEnabled()) {
+          log.warn("EyalsSAM.prepareMainTask : missing parameter value for : " + param);
         }
-        
-        return page;
+      } else {
+        // TODO validate
+        parameters = parameters.replaceAll(FREE_PARAM_DELIMITER + param + FREE_PARAM_DELIMITER, value);
+      }
+
     }
-	
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-		//File
-		name = config.getInitParameter("name");
-		String file = FileManager.LEGACYAPPS+"/"+name+"/"+config.getInitParameter("parameterfile.template");
-		executable = FileManager.LEGACYAPPS+"/"+name+"/"+config.getInitParameter("executable");
-		treesDir = config.getInitParameter("treesDir");
-		bc03Dir = config.getInitParameter("bc03Dir");
-		
-		StringBuilder sb = new StringBuilder();
-		try
-		{
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line;
-			while((line = reader.readLine()) != null)
-				sb.append(line).append("\n");
-			reader.close();
-		} catch(IOException e)
-		{
-			// TODO do something or initialise differently ?
-			throw new ServletException(e);
-		}
-		paramtemplate = sb.toString();
-		paramtemplate = paramtemplate.replaceAll(FIXED_PARAM_DELIMITER+"treesDir"+FIXED_PARAM_DELIMITER, treesDir);
-		paramtemplate = paramtemplate.replaceAll(FIXED_PARAM_DELIMITER+"bc03Dir"+FIXED_PARAM_DELIMITER, bc03Dir);
-		int index1 =0;
-		freeParameters = new ArrayList<String>();
-		while(true)
-		{
-		   index1 = paramtemplate.indexOf(FREE_PARAM_DELIMITER, index1);
-		   if(index1 == -1)
-			   break;
-		   index1+=FREE_PARAM_DELIMITER.length(); // TODO optimize
-		   int index2 = paramtemplate.indexOf(FREE_PARAM_DELIMITER, index1);
-		   String param = paramtemplate.substring(index1, index2);
-		   freeParameters.add(param);
-		   index1=index2+FREE_PARAM_DELIMITER.length();// TODO optimize
-		}
-	}
-    /**
-     * Perform the event from the given run context
-     * @param rootCtx root context
-     * @param runCtx  current run context
-     * @return boolean: true of the processing should continue, false if the job should be terminated
-     */
-    public boolean performTaskDone(final RootContext rootCtx, final RunContext runCtx) {
-    	boolean ok = true;
-    	if(MAIN_TASK.equals(runCtx.getName()))
-    	{
-    		//TODO add plotting tasks
-    		if(runCtx.getState() == RunState.STATE_FINISHED_OK)
-    		{
-    			ok = true;
-    			LocalLauncher.prepareChildJob(rootCtx, PLOT_TASK, plotCommand);
-    		} else {
-    			ok = false;
-    		}
-    	}
-    	return ok;
+    if (log.isDebugEnabled()) {
+      log.debug("workDir :" + workDir);
     }
+
+    final String wd = workDir.replace('\\', '/');
+    if (log.isDebugEnabled()) {
+      log.debug("workDir repl :" + workDir);
+    }
+
+    parameters = parameters.replaceAll(FIXED_PARAM_DELIMITER + "workingDir" + FIXED_PARAM_DELIMITER, wd);
+
+    File f = new File(workDir + "/esam_params");
+    FileWriter w = new FileWriter(f);
+    w.write(parameters);
+    w.flush();
+    w.close();
+
+    return new String[]{executable};
+  }
+
+  /**
+   * Return the job default folder default/
+   * @return
+   */
+  public String getName() {
+    return name;
+  }
+
+  protected String showJob(final HttpServletRequest request, final Integer id) {
+    String page = super.showJob(request, id);
+
+    final RootContext ctx = (RootContext) request.getAttribute("runContext");
+    if (ctx != null) {
+      File path;
+      switch (ctx.getState()) {
+        case STATE_RUNNING:
+          path = new File(ctx.getWorkingDir());
+          break;
+        case STATE_FINISHED_ERROR:
+        case STATE_FINISHED_OK:
+          path = new File(FileManager.ARCHIVE, ctx.getRelativePath());
+          break;
+        default:
+          path = null;
+      }
+      request.setAttribute("resultDir", path);
+    }
+
+    return page;
+  }
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    // TODO Auto-generated method stub
+    //File
+    name = config.getInitParameter("name");
+    String file = FileManager.LEGACYAPPS + "/" + name + "/" + config.getInitParameter("parameterfile.template");
+    executable = FileManager.LEGACYAPPS + "/" + name + "/" + config.getInitParameter("executable");
+    treesDir = config.getInitParameter("treesDir");
+    bc03Dir = config.getInitParameter("bc03Dir");
+
+    StringBuilder sb = new StringBuilder();
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        sb.append(line).append("\n");
+      }
+      reader.close();
+    } catch (IOException e) {
+      // TODO do something or initialise differently ?
+      throw new ServletException(e);
+    }
+    paramtemplate = sb.toString();
+    paramtemplate = paramtemplate.replaceAll(FIXED_PARAM_DELIMITER + "treesDir" + FIXED_PARAM_DELIMITER, treesDir);
+    paramtemplate = paramtemplate.replaceAll(FIXED_PARAM_DELIMITER + "bc03Dir" + FIXED_PARAM_DELIMITER, bc03Dir);
+    int index1 = 0;
+    freeParameters = new ArrayList<String>();
+    while (true) {
+      index1 = paramtemplate.indexOf(FREE_PARAM_DELIMITER, index1);
+      if (index1 == -1) {
+        break;
+      }
+      index1 += FREE_PARAM_DELIMITER.length(); // TODO optimize
+      int index2 = paramtemplate.indexOf(FREE_PARAM_DELIMITER, index1);
+      String param = paramtemplate.substring(index1, index2);
+      freeParameters.add(param);
+      index1 = index2 + FREE_PARAM_DELIMITER.length();// TODO optimize
+    }
+  }
+
+  /**
+   * Perform the event from the given run context
+   * @param rootCtx root context
+   * @param runCtx  current run context
+   * @return boolean: true of the processing should continue, false if the job should be terminated
+   */
+  public boolean performTaskDone(final RootContext rootCtx, final RunContext runCtx) {
+    boolean ok = true;
+    if (MAIN_TASK.equals(runCtx.getName())) {
+      //TODO add plotting tasks
+      if (runCtx.getState() == RunState.STATE_FINISHED_OK) {
+        ok = true;
+        LocalLauncher.prepareChildJob(rootCtx, PLOT_TASK, plotCommand);
+      } else {
+        ok = false;
+      }
+    }
+    return ok;
+  }
 }
--- trunk/JobRunner/src/org/ivoa/util/runner/LocalLauncher.java
+++ trunk/JobRunner/src/org/ivoa/util/runner/LocalLauncher.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.ivoa.util.JavaUtils;
 import org.ivoa.util.LogUtil;
 import org.ivoa.util.concurrent.FastSemaphore;
 import org.ivoa.util.concurrent.GenericRunnable;
@@ -28,45 +29,32 @@ public final class LocalLauncher {
 
   /** logger */
   protected static final Log log = LogUtil.getLogger();
-
   /** initial capacity for queue */
   public static final int INITIAL_QUEUE_CAPACITY = 100;
-
   /** job ID generator (counter) */
   private static final AtomicInteger JOBS_ID = new AtomicInteger(0);
-
   /** live job count */
   private static final AtomicInteger JOBS_LIVE = new AtomicInteger(0);
-
   /** total queued job count */
   private static final AtomicInteger JOBS_QUEUED = new AtomicInteger(0);
-
   /** total job count */
   private static final AtomicInteger JOBS_TOTAL = new AtomicInteger(0);
-
   /** remove policy for queue : default automatic remove after job finished */
   private static boolean QUEUE_MANUAL_REMOVE_JOBS = false;
-
   /** queue semaphore */
   private static final FastSemaphore QUEUE_SEM = new FastSemaphore(1);
-
   /** QUEUE for job management */
   private static final Map<Integer, RootContext> JOB_QUEUE = new LinkedHashMap<Integer, RootContext>(
-    INITIAL_QUEUE_CAPACITY);
-
+      INITIAL_QUEUE_CAPACITY);
   // ~ ----
   public static final int ILLEGAL_STATE_ERROR_CODE = 1;
-  
   //~ Members ----------------------------------------------------------------------------------------------------------
-
   /** last total logged */
   private int lastTotal = -1;
-
   /** last live logged */
   private int lastLive = -1;
 
   //~ Constructors -----------------------------------------------------------------------------------------------------
-
   /**
    * Forbidden Constructor
    */
@@ -74,7 +62,6 @@ public final class LocalLauncher {
   }
 
   //~ Methods ----------------------------------------------------------------------------------------------------------
-
   /**
    * Prepare thread executors
    * @see ThreadExecutors#startExecutors()
@@ -117,7 +104,7 @@ public final class LocalLauncher {
       log.warn("LocalLauncher.purgeTerminated : enter");
     }
 
-    int                    n    = 0;
+    int n = 0;
     final List<RootContext> list = getQueue();
 
     if (list != null) {
@@ -125,9 +112,9 @@ public final class LocalLauncher {
         log.info("LocalLauncher.purgeTerminated : queue size : " + list.size());
       }
 
-      final long now      = System.currentTimeMillis();
+      final long now = System.currentTimeMillis();
 
-      long       duration;
+      long duration;
 
       for (final RunContext job : list) {
         if ((job.getState() == RunState.STATE_FINISHED_ERROR) || (job.getState() == RunState.STATE_FINISHED_OK)) {
@@ -159,8 +146,8 @@ public final class LocalLauncher {
    * Logs the Launcher statistics
    */
   public void handle() {
-    final int live        = JOBS_LIVE.get();
-    final int total       = JOBS_TOTAL.get();
+    final int live = JOBS_LIVE.get();
+    final int total = JOBS_TOTAL.get();
 
     if ((live > lastLive) || (total > lastTotal)) {
       // fast simple barrier :
@@ -183,12 +170,12 @@ public final class LocalLauncher {
    * @return created job context
    */
   public static RootContext prepareMainJob(final String appName, final String owner, final String workingDir, final int bufferLines,
-                                          final String writeLogFile) {
+                                           final String writeLogFile) {
     if (log.isDebugEnabled()) {
       log.debug("LocalLauncher.prepareMainJob : enter");
     }
 
-    final Integer        id     = Integer.valueOf(JOBS_ID.incrementAndGet());
+    final Integer id = Integer.valueOf(JOBS_ID.incrementAndGet());
 
     final RootContext runCtx = new RootContext(appName, id, workingDir);
     runCtx.setOwner(owner);
@@ -210,11 +197,15 @@ public final class LocalLauncher {
    * @return created job context
    */
   public static RunContext prepareChildJob(final RootContext parent, final String name, final String[] command) {
+    if (JavaUtils.isEmpty(command)) {
+      throw new IllegalArgumentException("Invalid command parameter !");
+    }
+
     if (log.isDebugEnabled()) {
       log.debug("LocalLauncher.prepareJob : enter");
     }
 
-    final Integer        id     = Integer.valueOf(JOBS_ID.incrementAndGet());
+    final Integer id = Integer.valueOf(JOBS_ID.incrementAndGet());
 
     final ProcessContext runCtx = new ProcessContext(parent, name, id, command);
 
@@ -225,8 +216,8 @@ public final class LocalLauncher {
     }
 
     return runCtx;
-  }  
-  
+  }
+
   /**
    * Adds a job context in the queue and call the listener if job is pending.
    * The job will be executed by the Process Thread pool.
@@ -392,7 +383,6 @@ public final class LocalLauncher {
     private final JobListener listener;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
-
     /**
      * Constructor for the given job context and listener
      * @param rootCtx job context
@@ -404,7 +394,6 @@ public final class LocalLauncher {
     }
 
     //~ Methods --------------------------------------------------------------------------------------------------------
-
     /**
      * This method uses the job listener for the running & finished events and use the ProcessRunner to execute the job
      */
@@ -419,7 +408,7 @@ public final class LocalLauncher {
       boolean ok = true;
       try {
         // set running state :
-    	  rootCtx.setState(RunState.STATE_RUNNING);
+        rootCtx.setState(RunState.STATE_RUNNING);
 
         // call listener :
         listener.performJobEvent(rootCtx);
@@ -430,25 +419,26 @@ public final class LocalLauncher {
         int maxn = 100;
         RunContext child = null;
 
-        while(ok && rootCtx.hasNext() && n < maxn) {
-        	child = rootCtx.next();
-            executeTask(child);
-        	ok = listener.performTaskDone(rootCtx, child);
-        	n++;
+        while (ok && rootCtx.hasNext() && n < maxn) {
+          child = rootCtx.next();
+
+          executeTask(child);
+          ok = listener.performTaskDone(rootCtx, child);
+          n++;
         }
 
       } finally {
-    	  
-    	  rootCtx.getRing().add("Job '"+rootCtx.getName()+"'Ended.");
-    	  
+
+        rootCtx.getRing().add("Job '" + rootCtx.getName() + "'Ended.");
+
         // set finished state :
-    	  rootCtx.setState(ok ? RunState.STATE_FINISHED_OK : RunState.STATE_FINISHED_ERROR);
+        rootCtx.setState(ok ? RunState.STATE_FINISHED_OK : RunState.STATE_FINISHED_ERROR);
 
         // call listener :
         listener.performJobEvent(rootCtx);
 
         // remove job from queue :
-        if (! QUEUE_MANUAL_REMOVE_JOBS) {
+        if (!QUEUE_MANUAL_REMOVE_JOBS) {
           removeFromQueue(rootCtx.getId());
         }
       }
@@ -460,54 +450,56 @@ public final class LocalLauncher {
         log.debug("JobRunner - thread.run : exit");
       }
     }
-	  
-	  /**
-	   * This method uses the job listener for the running & finished events and use the ProcessRunner to execute the job
-	   * @return true if the execution was successful
-	   */
-	  private final void executeTask(final RunContext runCtx) {
-	    if (log.isDebugEnabled()) {
-	      log.debug("JobRunner.executeTask : enter : " + runCtx.getId());
-	    }
-	    
-	    int status = -1;
-	    try {
-	      // set running state :
-	      runCtx.setState(RunState.STATE_RUNNING);
-	
-	      // call listener :
-	      listener.performTaskEvent(runCtx.getParent(), runCtx);
-	
-	      // starts program & waits for its end (and std threads) :
-	      // uses a ring buffer for stdout/stderr :
-	      
-	      if(runCtx instanceof ProcessContext)
-	    	  status = ProcessRunner.execute((ProcessContext)runCtx);
-	      else
-	    	  status = ILLEGAL_STATE_ERROR_CODE;
-	    		  
-	      if (log.isInfoEnabled()) {
-	        log.info("JobRunner.run : process return status : " + status);
-	      }
-	
-	      // ring buffer is not synchronized because threads have finished their jobs in ProcessRunner.run(runCtx) :
-	      if (status == 0) {
-	        runCtx.getRing().add("Task '"+runCtx.getName()+"' Ended.");
-	      } else {
-	        runCtx.getRing().add(ProcessRunner.ERR_PREFIX, "Task Ended with a failure exit code : " + status + ".");
-	      }
-	    } finally {
-	      // set finished state :
-	      runCtx.setState((status == 0) ? RunState.STATE_FINISHED_OK : RunState.STATE_FINISHED_ERROR);
-	
-	      // call listener :
-	      listener.performTaskEvent(runCtx.getParent(), runCtx);
-	    }
-	
-	    if (log.isDebugEnabled()) {
-		      log.debug("JobRunner.executeTask : exit : " + runCtx.getId());
-	    }
-	  }
+
+    /**
+     * This method uses the job listener for the running & finished events and use the ProcessRunner to execute the job
+     * @return true if the execution was successful
+     */
+    private final void executeTask(final RunContext runCtx) {
+      if (log.isDebugEnabled()) {
+        log.debug("JobRunner.executeTask : enter : " + runCtx.getId());
+      }
+
+      int status = -1;
+      try {
+        // set running state :
+        runCtx.setState(RunState.STATE_RUNNING);
+
+        // call listener :
+        listener.performTaskEvent(runCtx.getParent(), runCtx);
+
+        // starts program & waits for its end (and std threads) :
+        // uses a ring buffer for stdout/stderr :
+
+        if (runCtx instanceof ProcessContext) {
+          status = ProcessRunner.execute((ProcessContext) runCtx);
+        } else {
+          status = ILLEGAL_STATE_ERROR_CODE;
+        }
+
+        if (log.isInfoEnabled()) {
+          log.info("JobRunner.run : process return status : " + status);
+        }
+
+        // ring buffer is not synchronized because threads have finished their jobs in ProcessRunner.run(runCtx) :
+        if (status == 0) {
+          runCtx.getRing().add("Task '" + runCtx.getName() + "' Ended.");
+        } else {
+          runCtx.getRing().add(ProcessRunner.ERR_PREFIX, "Task Ended with a failure exit code : " + status + ".");
+        }
+      } finally {
+        // set finished state :
+        runCtx.setState((status == 0) ? RunState.STATE_FINISHED_OK : RunState.STATE_FINISHED_ERROR);
+
+        // call listener :
+        listener.performTaskEvent(runCtx.getParent(), runCtx);
+      }
+
+      if (log.isDebugEnabled()) {
+        log.debug("JobRunner.executeTask : exit : " + runCtx.getId());
+      }
+    }
   }
 }
 //~ End of file --------------------------------------------------------------------------------------------------------
+
