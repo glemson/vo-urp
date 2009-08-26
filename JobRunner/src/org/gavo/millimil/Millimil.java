805406d6c805de835b352c128b2374988d0799a7
bourges.laurent@gmail.com <bourges.laurent@gmail.com@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-08-26 18:51:12 +0000

    format + status OK fixed

--- JobRunner/src/org/gavo/millimil/Millimil.java
+++ JobRunner/src/org/gavo/millimil/Millimil.java
@@ -28,128 +28,123 @@ import org.ivoa.util.runner.process.ProcessContext;
 import org.ivoa.web.servlet.JobServlet;
 import org.ivoa.web.servlet.JobStateException;
 
-public class Millimil extends JobServlet{
 
-	public static final String RUNQUERY_TASK = "runQuery";
-	public static final String RUNQUERY_RESULT_FILE = "result.csv";
-	public static final String RUNQUERY_ERROR_FILE = "error.txt";
-	public static final String RPLOT_TASK = "Rplot";
-	private String name;
-	private String executable;
-	private String baseURL;
-	private String Rcmd;
-	private String Rscript;
+public class Millimil extends JobServlet {
 
-	@Override
-	protected void initialiseMainJob(RootContext rootCtx, HttpServletRequest request) throws JobStateException
-	{
-        // add the first task in the root context :
-		try
-		{
-		String[] command = prepareQueryTask(rootCtx.getWorkingDir(), request);
-        LocalLauncher.prepareChildJob(rootCtx, RUNQUERY_TASK, command);
-		} catch(Exception e)
-		{
-			throw new JobStateException(e);
-		}
-	}
+  public static final String RUNQUERY_TASK = "runQuery";
+  public static final String RUNQUERY_RESULT_FILE = "result.csv";
+  public static final String RUNQUERY_ERROR_FILE = "error.txt";
+  public static final String RPLOT_TASK = "Rplot";
+  private String name;
+  private String executable;
+  private String baseURL;
+  private String Rcmd;
+  private String Rscript;
 
-    private String[] prepareQueryTask(final String workDir, HttpServletRequest req) throws IOException {
-		// TODO Auto-generated method stub
-		String sql = req.getParameter("SQL");
-		String url= baseURL+URLEncoder.encode(sql,"UTF-8");
-		
-		return new String[]{executable, "-O", RUNQUERY_RESULT_FILE,"\""+url+"\""};
-	}
-	
-    private void prepareRPlotTask(RootContext rootCtx)
-    {
-       String[] cmd = new String[]{Rcmd,"BATCH","--no-save",Rscript, "Rplot.log"};
-       LocalLauncher.prepareChildJob(rootCtx, RPLOT_TASK, cmd);
+  @Override
+  protected void initialiseMainJob(RootContext rootCtx, HttpServletRequest request) throws JobStateException {
+    // add the first task in the root context :
+    try {
+      String[] command = prepareQueryTask(rootCtx.getWorkingDir(), request);
+      LocalLauncher.prepareChildJob(rootCtx, RUNQUERY_TASK, command);
+    } catch (Exception e) {
+      throw new JobStateException(e);
+    }
+  }
+
+  private String[] prepareQueryTask(final String workDir, HttpServletRequest req) throws IOException {
+    // TODO Auto-generated method stub
+    String sql = req.getParameter("SQL");
+    String url = baseURL + URLEncoder.encode(sql, "UTF-8");
+
+    return new String[]{executable, "-O", RUNQUERY_RESULT_FILE, "\"" + url + "\""};
+  }
+
+  private void prepareRPlotTask(RootContext rootCtx) {
+    String[] cmd = new String[]{Rcmd, "BATCH", "--no-save", Rscript, "Rplot.log"};
+    LocalLauncher.prepareChildJob(rootCtx, RPLOT_TASK, cmd);
+  }
+
+  /**
+   * Return the application name
+   * @return
+   */
+  public String getName() {
+    return name;
+  }
+
+  protected String showJob(final HttpServletRequest request, final Integer id) {
+    String page = super.showJob(request, id);
+
+    final RootContext ctx = (RootContext) request.getAttribute("runContext");
+    if (ctx != null) {
+      File path;
+      switch (ctx.getState()) {
+        case STATE_RUNNING:
+          path = new File(ctx.getWorkingDir());
+          break;
+        case STATE_FINISHED_ERROR:
+        case STATE_FINISHED_OK:
+          path = new File(FileManager.ARCHIVE, ctx.getRelativePath());
+          break;
+        default:
+          path = null;
+      }
+      request.setAttribute("resultDir", path);
     }
 
-    /**
-     * Return the application name
-     * @return
-     */
-    public String getName() {
-      return name;
-    }
+    return page;
+  }
 
-    protected String showJob(final HttpServletRequest request, final Integer id) {
-        String page = super.showJob(request, id);
-        
-        final RootContext ctx = (RootContext)request.getAttribute("runContext");
-        if(ctx != null) {
-	        File path; 
-	        switch(ctx.getState())
-	        {
-	        	case STATE_RUNNING:
-	        		path = new File(ctx.getWorkingDir());
-	        		break;
-	        	case STATE_FINISHED_ERROR :
-	        	case STATE_FINISHED_OK :
-	        		path = new File(FileManager.ARCHIVE, ctx.getRelativePath());
-	        		break;
-	       		default:
-	       			path = null;
-	        }
-	        request.setAttribute("resultDir", path);
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    name = config.getInitParameter("name");
+    executable = config.getInitParameter("executable");
+    baseURL = config.getInitParameter("baseURL");
+
+    Rcmd = config.getInitParameter("Rcmd");
+    Rscript = FileManager.LEGACYAPPS + "/" + name + "/" + config.getInitParameter("Rscript");
+  }
+
+  /**
+   * Perform the event from the given run context
+   * @param rootCtx root context
+   * @param runCtx  current run context
+   * @return boolean: true of the processing should continue, false if the job should be terminated
+   */
+  public boolean performTaskDone(final RootContext rootCtx, final RunContext runCtx) {
+    boolean ok = true;
+    if (RUNQUERY_TASK.equals(runCtx.getName())) {
+      if (runCtx.getState() == RunState.STATE_FINISHED_OK) {
+        File result = new File(runCtx.getWorkingDir() + "/" + RUNQUERY_RESULT_FILE);
+        if (result.exists()) {
+          try {
+            BufferedReader reader = new BufferedReader(new FileReader(result));
+            String line = reader.readLine();
+            if (line != null && line.startsWith("#OK")) {
+              ok = true;
+            } else {
+              ok = false;
+            }
+            reader.close();
+            if (!ok) {
+              FileManager.moveFile(result, new File(runCtx.getWorkingDir() + "/" + RUNQUERY_ERROR_FILE));
+            } else {
+              prepareRPlotTask(runCtx.getParent());
+            }
+          } catch (IOException e) {
+            // TODO TBD should we throw an exception here?
+            ok = false;
+          }
         }
-        
-        return page;
+      } else {
+        ok = false;
+      }
+    } else if (RPLOT_TASK.equals(runCtx.getName())) {
+      if (runCtx.getState() != RunState.STATE_FINISHED_OK) {
+        ok = false;
+      }
     }
-	
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		name = config.getInitParameter("name");
-		executable = config.getInitParameter("executable");
-		baseURL=config.getInitParameter("baseURL");
-		
-		Rcmd = config.getInitParameter("Rcmd");
-		Rscript = FileManager.LEGACYAPPS+"/"+name+"/"+config.getInitParameter("Rscript");
-	}
-    /**
-     * Perform the event from the given run context
-     * @param rootCtx root context
-     * @param runCtx  current run context
-     * @return boolean: true of the processing should continue, false if the job should be terminated
-     */
-    public boolean performTaskDone(final RootContext rootCtx, final RunContext runCtx){
-    	boolean ok = false;
-    	if(RUNQUERY_TASK.equals(runCtx.getName()))
-    	{
-    		File result = new File(runCtx.getWorkingDir()+"/"+RUNQUERY_RESULT_FILE);
-    		if(result.exists())
-    		{
-    			try
-    			{
-    				BufferedReader reader = new BufferedReader(new FileReader(result));
-    				String line = reader.readLine();
-    				if(line != null && line.startsWith("#OK"))
-    					ok = true;
-    				else 
-    					ok = false;
-    				reader.close();
-    				if(!ok)
-    					FileManager.moveFile(result, new File(runCtx.getWorkingDir()+"/"+RUNQUERY_ERROR_FILE));
-    				else
-    					prepareRPlotTask(runCtx.getParent());
-    			} catch(IOException e)
-    			{
-    				// TODO TBD should we throw an exception here?
-    				ok = false;
-    			}
-    		}
-    		else
-    			ok = false;
-    	} else if(RPLOT_TASK.equals(runCtx.getName()))
-    	{
-    		if(runCtx.getState() == RunState.STATE_FINISHED_OK)
-    		{
-    		    	
-    		}
-    	}
-    	return ok;
-    }
+    return ok;
+  }
 }
