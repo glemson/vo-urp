839d91e4a87b38c5b1e8911114f24b223dcb9b0e
gerard.lemson <gerard.lemson@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2009-11-19 08:57:48 +0000

    experimental code for a generic jobrunner. checked in for future further development.

---
+++ JobRunner/src/org/ivoa/runner/apps/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://vo-urp.googlecode.com/LegacyApps", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.ivoa.runner.apps;
