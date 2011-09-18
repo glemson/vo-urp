0334d5e0a3cd925495a9274800418c0f898c423b
bourges.laurent@gmail.com <bourges.laurent@gmail.com@cfaf569f-ed4e-0410-9bfc-33f004d4b02c>
2011-09-18 21:01:07 +0000

    fixed admindb script (tomcat roles) for postgres database

--- trunk/webapp/vo-urp-browser/ddl/postgres/admindb.sql
+++ trunk/webapp/vo-urp-browser/ddl/postgres/admindb.sql
@@ -1,26 +1,30 @@
-create table users (username varchar(128) not null, password varchar(128) not null, createDate datetime default current_timestamp);
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS user_roles;
+
+create table users (username varchar(128) not null, password varchar(128) not null, createDate TIMESTAMP default current_timestamp);
 create table roles (rolename varchar(128) not null);
 create table user_roles (username varchar(128) not null, rolename varchar(128) not null);
 
 -- data :
-insert into roles 
+insert into roles (rolename)
  select '@webapp.member.role@' 
-  where not exists select * from roles where rolename= '@webapp.member.role@';
+  where not exists (select 0 from roles where rolename= '@webapp.member.role@');
 
-insert into roles
+insert into roles (rolename)
  select '@webapp.admin.role@' 
-  where not exists select * from roles where rolename= '@webapp.admin.role@';
+  where not exists (select 0 from roles where rolename= '@webapp.admin.role@');
 
 -- next should maybe check that user has proper password if it exists?
-insert into users (username, password) 
+insert into users (username, password)
  select '@webapp.admin.user@', '@webapp.admin.password@' 
-  where not exists select * from users where rolename= '@webapp.admin.user@';
+  where not exists (select 0 from users where username= '@webapp.admin.user@');
 
 insert into user_roles (username, rolename) 
  select '@webapp.admin.user@', '@webapp.admin.role@' 
-  where not exists select * from user_roles where username= '@webapp.admin.user@' and rolename = '@webapp.admin.role@';
+  where not exists (select 0 from user_roles where username= '@webapp.admin.user@' and rolename = '@webapp.admin.role@');
 
 insert into user_roles (username, rolename) 
  select '@webapp.admin.user@', '@webapp.member.role@' 
-  where not exists select * from user_roles where username= '@webapp.admin.user@' and rolename = '@webapp.member.role@';
+  where not exists (select 0 from user_roles where username= '@webapp.admin.user@' and rolename = '@webapp.member.role@');
 
