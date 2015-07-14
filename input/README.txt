3a2146269f1a67eee029b7efa949aabff44da7bc
glemson <gerard.lemson@gmail.com>
2015-07-14 12:13:00 -0400

    removed input

--- input/README.txt
+++
@@ -1,1 +0,0 @@
-This directory contains sample projects, each in its own sub directory.
--- input/template/test/java/Test.java
+++
@@ -1,8 +0,0 @@
-public final class Test {
-  public static void main(String[] args)
-  {
-    System.out.println("=====================================================");
-    System.out.println("This is a test method for the VO-URP template project");
-    System.out.println("=====================================================");
-  }
-}
